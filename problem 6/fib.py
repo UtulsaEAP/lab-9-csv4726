4cc63d9cd6f19468a6a4b586c032d6acc8d28721
csv4726 <csv4726@utulsa.edu>
2024-10-31 10:57:36 -0500

    all Q1-6 pass

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,7 +1,20 @@
 def fibonacci(n):
     
     #write your code here
-    return
+        # Return -1 for any negative index
+    if n < 0:
+        return -1
+    # Handle base cases
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # Calculate Fibonacci for other cases
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
 
 if __name__ == '__main__':
     start_num = int(input())
